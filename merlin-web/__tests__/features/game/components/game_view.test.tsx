004378734a0bde024346a1d3c047d1760616216c
Greg Jones <4853264+gregjones07ba@users.noreply.github.com>
2025-11-28 18:36:18 -0800

    Test that renders the GameView but doesn't check anything yet.

---
+++ merlin-web/__tests__/features/game/components/game_view.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import {render, screen} from '@testing-library/react'
+
+import GameView from '../../../../src/features/game/components/game_view'
+import GameController from '../../../../src/features/game/controllers/game_controller'
+
+test('it displays a text input', () => {
+    const controller = new GameController();
+    render(<GameView controller={controller}/>);
+});
